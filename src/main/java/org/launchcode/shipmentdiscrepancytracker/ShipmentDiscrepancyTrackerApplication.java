91998db284e810b963ea7a9c0bd97f624add622b
Kirsti Schaffer <kirstischaffer@gmail.com>
2021-03-05 18:51:43 -0600

    first commit

---
+++ src/main/java/org/launchcode/shipmentdiscrepancytracker/ShipmentDiscrepancyTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.shipmentdiscrepancytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmentDiscrepancyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipmentDiscrepancyTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/shipmentdiscrepancytracker/ShipmentDiscrepancyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.shipmentdiscrepancytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentDiscrepancyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
